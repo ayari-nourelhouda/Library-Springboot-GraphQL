a6b7612d95a00c83cd0d76c3f1b50526c12c5062
nayari <nayari@vermeg.com>
2022-05-22 23:38:06 +0100

    BookGraphQL : First Edition

--- src/main/java/com/projectLib/GraphQLLibrary/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projectLib.GraphQLLibrary.repository;
-
-import com.projectLib.GraphQLLibrary.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
